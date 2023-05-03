fb05c3e0bd8f1a53a301f8be56a9883b01c9682d
JEfloyd1567 <juanestebanfloydjimenez@gmail.com>
2023-05-03 17:39:23 -0500

    los estilos se corrigieron

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,49 +1,57 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import './Login.css';
 
 export function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSubmit = async event => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
-        const response = await axios.post('/login', {}, {
-            auth:{
-            username: username,
-            password: password
-            }
-        });
+      const response = await axios.post('/login', {}, {
+        auth: {
+          username: username,
+          password: password
+        }
+      });
     }
-    catch(error) {
-        console.error(error);
+    catch (error) {
+      console.error(error);
     }
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Correo Electrónico:
-                <input
-                type='username'
-                value={username}
-                onChange={event => setUsername(event.target.value)}
-                />
-            </label>
-            <label>
-                Contraseña:
-                <input
-                type='password'
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-                />
-            </label>
-            <button type='submit'>Iniciar sesión</button>
-            <p>
-                ¿No tienes una cuenta?{''}
-                <Link to='/register'>Registrate aquí</Link>
-            </p>
-        </form>
-    );
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2 className="login-title">Iniciar sesión</h2>
+        <div className="login-input-container">
+          <label className="login-label">
+            Usuario:
+            <input
+              type='username'
+              value={username}
+              onChange={event => setUsername(event.target.value)}
+              className="login-input"
+            />
+          </label>
+          <label className="login-label">
+            Contraseña:
+            <input
+              type='password'
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              className="login-input"
+            />
+          </label>
+        </div>
+        <button type='submit' className="login-button">Iniciar sesión</button>
+        <p className="login-signup-link">
+          ¿No tienes una cuenta?{' '}
+          <Link to='/register' className="login-link">Registrate aquí</Link>
+        </p>
+      </form>
+    </div>
+  );
 }
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,45 +1,46 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate  } from 'react-router-dom';
-
-
+import { useNavigate } from 'react-router-dom';
+import './register.css';
 
 export function Register() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-
-    const handleSubmit = async event => {
-        event.preventDefault();
-        navigate('/');
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    navigate('/');
     try {
-        const auth = {username, password};
-        const response = await axios.post('/register', { auth });
+      const auth = { username, password };
+      const response = await axios.post('/register', { auth });
+    } catch (error) {
+      console.error(error);
     }
-    catch(error) {
-        console.error(error);
-    }
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Usuario:
-                <input
-                type='username'
-                value={username}
-                onChange={event => setUsername(event.target.value)}
-                />
-            </label>
-            <label>
-                Contraseña:
-                <input
-                type='password'
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-                />
-            </label>
-            <button type='submit'>Registrarse</button>
-        </form>
-    );
+  };
+  return (
+    <div className="register-container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label>Usuario:</label>
+          <input
+            type="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <button type="submit">Registrarse</button>
+        </div>
+      </form>
+    </div>
+  );
 }
