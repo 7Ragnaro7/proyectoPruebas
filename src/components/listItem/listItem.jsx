34aa05494ef252839391b2eae3e8b36db9770f3b
JEfloyd1567 <juanestebanfloydjimenez@gmail.com>
2023-05-15 21:43:30 -0500

    fix listItem, add style

--- src/components/listItem/listItem.jsx
+++ src/components/listItem/listItem.jsx
@@ -1,61 +1,62 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import API_BASE_URL from '../../config/baseUrl'
-
+import API_BASE_URL from '../../config/baseUrl';
+import './listItem.css';
 
 export function ListItem() {
-    const navigate = useNavigate();
-    const jwt_token = localStorage.getItem('token');
-    let parsed_json;
-    let nombresCosto = [];
-    let mostrarElementos = [];
-    let minValue = 9999;
-    const options = {
-        method: 'GET',
-        url: API_BASE_URL+'/products',
-        headers: {
-            'x-access-tokens': jwt_token
-        }
-    };
-    async function fetchData() {
-        try {
-            const response = await axios.request(options);
-            return response;
-        } catch (error) {
-            navigate('/welcome');
-        }
+  const navigate = useNavigate();
+  const jwt_token = localStorage.getItem('token');
+  const [elementosMostrados, setElementosMostrados] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (!jwt_token || jwt_token === '') {
+      navigate('/');
+      return;
     }
-    useEffect(() => {
-        if (!jwt_token || jwt_token === '') {
-            navigate('/');
+
+    async function fetchData() {
+      try {
+        const options = {
+          method: 'GET',
+          url: API_BASE_URL + '/products',
+          headers: {
+            'x-access-tokens': jwt_token
+          }
+        };
+
+        const response = await axios.request(options);
+
+        if (response.status === 200) {
+          const lista = response.data.list_of_products;
+          const nombresCosto = lista.map((elemento) => {
+            return { nombre: elemento.name, precio: elemento.price };
+          });
+
+          setElementosMostrados(nombresCosto);
+          setIsLoading(false);
         }
-        try {
-            fetchData().then(response => {
-                if (response.status === 200) {
-                    const lista = response.data.list_of_products;
-                    const obj = JSON.stringify(lista);
-                    parsed_json = JSON.parse(obj);
-                    if(minValue > Object.keys(parsed_json).length){
-                        minValue = Object.keys(parsed_json).length;
-                    }
-                }
-                for(let i = 0; i < Object.keys(parsed_json).length; i++){
-                    nombresCosto.push([parsed_json[i].name, parsed_json[i].price]);
-                }
-                if(nombresCosto.length == minValue){
-                    mostrar = nombresCosto;
-                }
-                console.log(mostrarElementos);
-            }
-            );
-        } catch (error) {
-            navigate('/welcome');
-        }
-    })
-    return (
-        <div className="add-item-container">
-            <pre>Hola</pre>
+      } catch (error) {
+        navigate('/welcome');
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div className="list-item-container">
+      {elementosMostrados.map((elemento, index) => (
+        <div key={index} className="elemento">
+          <p className="nombre">{elemento.nombre}</p>
+          <p className="precio">{elemento.precio}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
